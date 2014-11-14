37199998e88b9ae6ac4533c3d7735cacc24725a8
unknown <Agatha Maison>
2014-11-14 13:29:43 +0000

    connecting to database and displaying routes

--- src/v/i/p_software/VIP_SOFTWARE.java
+++ src/v/i/p_software/VIP_SOFTWARE.java
@@ -30,14 +30,22 @@ public class VIP_SOFTWARE {
                     break;
                 }
             }
-        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(VIP_SOFTWARE.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(ADMIN_LOGIN.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(ADMIN_LOGIN.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(ADMIN_LOGIN.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(ADMIN_LOGIN.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
-        java.awt.EventQueue.invokeLater(() -> {
-            new WELCOME_INTERFACE().setVisible(true);
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new WELCOME_INTERFACE().setVisible(true);
+            }
         });
-    }    
+    }   
 }
