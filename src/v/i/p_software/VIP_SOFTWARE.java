cbd8d56292af0ee32b87a2a2c4747d876a896a5b
Michael Annor <michael.annor@ashesi.edu.gh>
2014-11-18 22:45:35 +0000

    first commit
    This is the first commit after importing to netbeans and attempting to
    resolve issues

--- src/v/i/p_software/VIP_SOFTWARE.java
+++ src/v/i/p_software/VIP_SOFTWARE.java
@@ -14,7 +14,7 @@ package v.i.p_software;
  */
 public class VIP_SOFTWARE {
 
-    /**
+        /**
      * @param args the command line arguments
      */
     public static void main(String args[]) {
@@ -30,14 +30,22 @@ public class VIP_SOFTWARE {
                     break;
                 }
             }
-        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(VIP_SOFTWARE.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(WELCOME_INTERFACE.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(WELCOME_INTERFACE.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(WELCOME_INTERFACE.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(WELCOME_INTERFACE.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
-        java.awt.EventQueue.invokeLater(() -> {
-            new WELCOME_INTERFACE().setVisible(true);
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new WELCOME_INTERFACE().setVisible(true);
+            }
         });
-    }    
+    }
 }
