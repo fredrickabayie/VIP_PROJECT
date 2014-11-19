919e32bcb52a34e812c3213ded55b6bdf359e972
Michael Annor <michael.annor@ashesi.edu.gh>
2014-11-19 14:21:41 +0000

    person class added
    complete person class with accessor and mutator methods

--- src/v/i/p_software/Person.java
+++ src/v/i/p_software/Person.java
@@ -10,5 +10,59 @@ package v.i.p_software;
  * @author michaelannor
  */
 public class Person {
+    private String firstName;
+    private String surname;
+    private String personID;
+    private int phoneNumber;
+    private char gender;
+    private String email;
+    
+    void setFirstName(String fn) {
+        firstName = fn;
+    }
+    
+    void setSurname(String sn) {
+        surname = sn;
+    }
+    
+    void setID(String id) {
+        personID = id;
+    }
+    
+    void setPhoneNumber(int num) {
+        phoneNumber = num;
+    }
+    
+    void setGender(char gen) {
+        gender = gen;
+    }
+    
+    void setEmail(String mail) {
+        email = mail;
+    }
+    
+    String getFirstName() {
+        return firstName;
+    }
+    
+    String getSurname() {
+        return surname;
+    }
+    
+    String getID() {
+        return personID;
+    }
+    
+    int getPhoneNumber() {
+        return phoneNumber;
+    }
+    
+    char getGender() {
+        return gender;
+    }
+    
+    String setEmail() {
+        return email;
+    }
     
 }
