79a9764dd3e33282520ca7fabb52705ccabff374
Michael Annor <michael.annor@ashesi.edu.gh>
2014-11-26 05:52:32 +0000

    completed Person crud

--- src/v/i/p_software/Person.java
+++ src/v/i/p_software/Person.java
@@ -12,7 +12,7 @@ package v.i.p_software;
 public class Person {
     private String firstName;
     private String surname;
-    private String personID;
+    private int personID;
     private int phoneNumber;
     private char gender;
     private String email;
@@ -25,7 +25,7 @@ public class Person {
         surname = sn;
     }
     
-    void setID(String id) {
+    void setID(int id) {
         personID = id;
     }
     
@@ -49,7 +49,7 @@ public class Person {
         return surname;
     }
     
-    String getID() {
+    int getID() {
         return personID;
     }
     
@@ -61,7 +61,7 @@ public class Person {
         return gender;
     }
     
-    String setEmail() {
+    String getEmail() {
         return email;
     }
     
--- src/v/i/p_software/PersonCrud.java
+++ src/v/i/p_software/PersonCrud.java
@@ -18,24 +18,90 @@ import java.io.*;
 import java.sql.*;
 
 public class PersonCrud {
+    
+    java.sql.Connection conn = null;
 
-java.sql.Connection conn = null;
-
-public void listAll(){
+    public void listAll(){
  try {
   java.sql.Statement s = conn.createStatement();
-  java.sql.ResultSet r = s.executeQuery("SELECT * FROM products");
+  java.sql.ResultSet r = s.executeQuery("SELECT * FROM person");
   while(r.next()) {
    System.out.println (
-    r.getString("Name") + " \t" +
-    r.getString("price") + " \t" +
-    r.getString("manufacturer") );
+    r.getInt("person_id") + " \t" +
+    r.getString("firstname") + " \t" +
+    r.getString("lastname") + " \t" +
+    r.getInt("age") + " \t" +
+    r.getInt("telno") + " \t" +
+    r.getChar("gender") + " \t" +
+    r.getString("email") );
   }
  }
  catch (Exception e) {
-  System.out.println(e);
-  System.exit(0);
+  System.out.println("Error" + e.toString());
+  //System.exit(0);
  }
+}    
+
+public void addPerson(Person pers){
+        //int id, string fn, string ln, int age, int telno
+ try{
+    PreparedStatement p= DBCudView.conn.prepareStatement("Insert Into person set person_id=?, "
+            + "firstname=?, lastname=? ,age =?, telno =?, gender =?, email =?");
+    p.setInt(1, pers.getID());
+    p.setString(2, pers.getFirstName());
+    p.setString(3, pers.getSurname());
+    p.setInt(4, pers.getAge());
+    p.setInt(5, pers.getPhoneNumber());
+    p.setChar(6, pers.getGender());
+    p.setString(7, pers.getEmail());
+    p.execute();  //use execute if no results expected back
+    }catch(Exception e){
+        System.out.println("Error"+e.toString());
+        //dialog
+        return;        
+    }
 }
+
+
+public void updatePerson(Person pers){
+    int oldKey = Integer.parseInt(pers.getID);
+      
+    try{
+    PreparedStatement p= DBCudView.conn.prepareStatement("Update person set person_id=?, "
+            + "firstname=?, lastname=? ,age =?, telno =?, gender =?, email =? Where person_id=?");
+   p.setInt(1, pers.getID());
+    p.setString(2, pers.getFirstName());
+    p.setString(3, pers.getSurname());
+    p.setInt(4, pers.getAge());
+    p.setInt(5, pers.getPhoneNumber());
+    p.setChar(6, pers.getGender());
+    p.setString(7, pers.getEmail());
+
+    //figure out how to do where:
+    p.setInt(8, oldKey);
+    p.execute();  //use execute if no results expected back
+    }catch(Exception e){
+        System.out.println("Error"+e.toString());
+        //dialog
+        return;        
+    }
     
+}
+
+public void deletePerson(Person pers){
+    
+ try{
+    PreparedStatement p= DBCudView.conn.prepareStatement("Delete from person Where person_id=?");
+    p.setInt(1, pers.getID);
+    p.execute();  //use execute if no results expected back
+    
+    
+    }catch(Exception e){
+        System.out.println("Error"+e.toString());
+        //dialog
+        return;        
+    }
+}
+
+
 }
